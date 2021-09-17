4bd050011edfbe678511c910ee27a82f817f4fc5
Bonyoze <ravaloni.emv@gmail.com>
2021-09-17 17:36:23 -0400

    Update tictactoe.js

--- src/commands/games/tictactoe.js
+++ src/commands/games/tictactoe.js
@@ -2,22 +2,31 @@ const bot = require("../../bot.js"),
 { SlashCommandBuilder } = require("@discordjs/builders"),
 { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
 
-const buildRow = (a, b, c) => {
-  return new MessageActionRow()
-    .addComponents(
-      new MessageButton()
-        .setCustomId(a)
-        .setLabel(" ")
-        .setStyle("SECONDARY"),
-      new MessageButton()
-        .setCustomId(b)
-        .setLabel(" ")
-        .setStyle("SECONDARY"),
-      new MessageButton()
-        .setCustomId(c)
-        .setLabel(" ")
-        .setStyle("SECONDARY")
-    );
+const buildBoard = (data, disabled) => {
+  let rows = [],
+  index = 0;
+
+  for (let x = 0; x < 3; x++) {
+    const row = new MessageActionRow();
+
+    for (let y = 0; y < 3; y++) {
+      index++;
+
+      const val = data[index];
+
+      row.addComponents(
+        new MessageButton()
+          .setCustomId(`${index}`)
+          .setLabel(val === undefined ? " " : val === false ? "X" : val === true && "O")
+          .setStyle(val === undefined ? "SECONDARY" : val === false ? "PRIMARY" : val === true && "DANGER")
+          .setDisabled(val !== undefined || disabled === true)
+      );
+    }
+
+    rows.push(row);
+  }
+
+  return rows;
 }
 
 module.exports = {
@@ -96,40 +105,148 @@ module.exports = {
       default:
         switch (command) {
           case "play":
-            const user = interaction.user,
+            let user = interaction.user,
             opponent = interaction.options.getUser("opponent");
 
-            const joinId = `tictactoe_join_${user.id}`,
-            joinFilter = i => {
-              console.log("filter: ", i);
-              i.customId === joinId && i.user.id === opponent.id
-            },
-            joinCollector = interaction.channel.createMessageComponentCollector({ joinFilter, max: 1 });
-
-            joinCollector.on("collect", async i => {
-              console.log("collect: ", i);
-            });
+            const embed = new MessageEmbed()
+              .setColor("#000000")
+              .setAuthor("Tic-Tac-Toe", bot.client.user.displayAvatarURL({ format: "png", dynamic: true }))
+              .setDescription(`*Waiting For ${opponent ? `<@${opponent.id}>` : "An Opponent"} To Join...*`)
+              .addField("**Players**", `\`🔵\`<@${user.id}>\n${opponent ? `\`🔴\`<@${opponent.id}>` : "`🔴`\\❔\\❔\\❔"}`)
 
             await interaction.reply(
               {
-                embeds: [
-                  new MessageEmbed()
-                    .setColor("#000000")
-                    .setAuthor("Tic-Tac-Toe", bot.client.user.displayAvatarURL({ format: "png", dynamic: true }))
-                    .setTitle("`Waiting for opponent to join...`")
-                    .setDescription(`<@${user.id}> vs. <@${opponent.id}>`)
-                ],
+                embeds: [ embed ],
                 components: [
                   new MessageActionRow()
                     .addComponents(
                       new MessageButton()
-                        .setCustomId(joinId)
+                        .setCustomId("join")
                         .setLabel("Join")
-                        .setStyle("PRIMARY")
+                        .setStyle("SUCCESS")
                     )
                 ]
               }
             );
+            
+            // join btn collector
+            const joinCollector = interaction.channel.createMessageComponentCollector({
+              filter: i => i.message.interaction.id === interaction.id && i.customId === "join",
+              time: 900000
+            })
+              .on("collect", async i => {
+                if (!opponent) {
+                  opponent = i.user;
+                } else if (i.user.id !== opponent.id) return await i.deferUpdate();
+
+                joinCollector.stop();
+
+                const players = [
+                  user,
+                  opponent
+                ],
+                winStates = [
+                  // horizontal
+                  [ 1, 2, 3 ],
+                  [ 4, 5, 6 ],
+                  [ 7, 8, 9 ],
+                  // vertical
+                  [ 1, 4, 7 ],
+                  [ 2, 5, 8 ],
+                  [ 3, 6, 9 ],
+                  // diagonal
+                  [ 1, 5, 9 ],
+                  [ 3, 5, 7]
+                ];
+
+                let board = [],
+                turn = false,
+                gameNum = 1;
+
+                embed
+                  .setDescription(`**Current Game: **\`${gameNum}\`\n\n**Turn: **\`🔵\`<@${user.id}>`)
+                  .fields[0].value = `\`🔵\`<@${user.id}>\n\`🔴\`<@${opponent.id}>`;
+
+                await i.update(
+                  {
+                    embeds: [ embed ],
+                    components: buildBoard(board)
+                  }
+                );
+
+                // playing btn collector
+                const btnCollector = interaction.channel.createMessageComponentCollector({
+                  filter: i => i.message.interaction.id === interaction.id
+                })
+                  .on("collect", async i => {
+                    const ply = players[+ turn];
+
+                    if (i.user.id === ply.id) {
+                      board[Number(i.customId)] = turn;
+
+                      // check if won
+                      const win = (
+                        () => {
+                          for (const state of winStates) {
+                            let valid = true;
+
+                            for (let i = 0; i < 3; i++) {
+                              if (board[state[i]] !== turn) {
+                                valid = false;
+                                break;
+                              }
+                            }
+
+                            if (valid) return true;
+                          }
+                          return false;
+                        }
+                      )();
+
+                      if (win) {
+                        embed.setDescription(`\`${turn ? "🔴" : "🔵"}\`<@${ply.id}> Wins!\n\n**Games Played: **\`${gameNum}\``);
+
+                        btnCollector.stop();
+                      } else {
+                        if (Object.keys(board).length === 9) { // restart board
+                          gameNum++;
+                          board = [];
+                        }
+
+                        turn = !turn;
+                        embed.setDescription(`**Current Game: **\`${gameNum}\`\n\n**Turn: **\`${turn ? "🔴" : "🔵"}\`<@${players[+ turn].id}>`);
+                      }
+
+                      await i.update(
+                        {
+                          embeds: [ embed ],
+                          components: buildBoard(board, win)
+                        }
+                      );
+                    } else await i.deferUpdate();
+                  });
+              })
+              .on("end", async (i, reason) => {
+                if (reason === "time") {
+                  embed.setFooter("⚠️ Game cancelled due to inactivity");
+
+                  await interaction.editReply(
+                    {
+                      embeds: [ embed ],
+                      components: [
+                        new MessageActionRow()
+                          .addComponents(
+                            new MessageButton()
+                              .setCustomId("join")
+                              .setLabel("Join")
+                              .setStyle("SUCCESS")
+                              .setDisabled(true)
+                          )
+                      ]
+                    }
+                  );
+                }
+              })
 
             break;
           case "cancel":
